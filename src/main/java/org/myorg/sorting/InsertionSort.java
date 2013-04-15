41c1db6c13c54f3e369e46d4f0868702f9607557
Jia Lu <jlu1985@gmail.com>
2013-04-15 12:19:15 -0400

    added InsertionSort and test case

---
+++ src/main/java/org/myorg/sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+package org.myorg.sorting;
+
+/**
+ * 
+ * @author Jia Lu
+ * 
+ */
+public class InsertionSort {
+	public void sort(int[] list) {
+		int valueToInsert;
+		int holePos;
+		for (int i = 1; i < list.length; i++) {
+			valueToInsert = list[i];
+			holePos = i;
+			while (holePos > 0 && (valueToInsert < list[holePos - 1])) {
+				list[holePos] = list[holePos - 1];
+				holePos = holePos - 1;
+			}
+			list[holePos] = valueToInsert;
+		}
+	}
+}
